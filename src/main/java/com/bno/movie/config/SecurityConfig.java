04bd2219436a033b8425131d4c0fb3fb8e15a836
Cheonho <ghtp8571@naver.com>
2025-03-19 17:33:43 +0900

    feat: cors / security 설정

---
+++ src/main/java/com/bno/movie/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.bno.movie.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/**","/*"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
---
+++ src/main/java/com/bno/movie/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.bno.movie.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user/")
+public class UserController {
+
+    @PostMapping("login")
+    public ResponseEntity<?> login() {
+        List<String> items = new ArrayList<>();
+        items.add("test1");
+        items.add("test2");
+        items.add("test3");
+        return ResponseEntity.ok().body(items);
+    }
+}
